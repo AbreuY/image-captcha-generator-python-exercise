a5a7398e3472bdfbe23660ee4aac88abda150ca8
AbreuY <yolfran12@gmail.com>
2022-10-09 15:16:36 +0000

    :new: app example file

---
+++ app.py
@@ -0,0 +1,11 @@
+from captcha.image import ImageCaptcha
+import random
+import string
+
+# Set image size
+image = ImageCaptcha(width=300, height=100)
+# Generate random uppercase text
+letters = string.ascii_uppercase
+captcha_text = ''.join(random.choice(letters) for i in range(5)) 
+# Set random text for captcha
+image.write(captcha_text, 'captcha.png')
